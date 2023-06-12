36c35dcd48387ac0459554a1fe9ad61c276f90d8
blueprint-12 <blueprint12@daum.net>
2023-06-12 22:37:18 +0900

    feat: 라우터 설정
    -react router dom v6

---
+++ src/router.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import Home from "@pages/Home";
+
+const SignIn = React.lazy(() => import("@pages/SignIn")); //로그인
+const SignUp = React.lazy(() => import("@pages/SignUp")); //회원가입
+const Content = React.lazy(() => import("@pages/Content")); //Todos가 담긴 페이지
+const NotFound = React.lazy(() => import("@pages/NotFound"));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/signin",
+        element: <SignIn />,
+      },
+      {
+        path: "/signup",
+        element: <SignUp />,
+      },
+      {
+        path: "/todo",
+        element: <Content />,
+      },
+      {
+        path: "*",
+        element: <NotFound />,
+      },
+    ],
+  },
+]);
+
+export default router;
