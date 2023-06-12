a3f5be6e57eb352e720f837b7c28c1537a354477
blueprint-12 <blueprint12@daum.net>
2023-06-13 03:18:50 +0900

    style: lazy load 말고 기본으로 해보기

--- src/router.tsx
+++ src/router.tsx
@@ -3,10 +3,15 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import Home from "@pages/Home";
 
-const SignIn = React.lazy(() => import("@pages/SignIn")); //로그인
-const SignUp = React.lazy(() => import("@pages/SignUp")); //회원가입
-const Content = React.lazy(() => import("@pages/Content")); //Todos가 담긴 페이지
-const NotFound = React.lazy(() => import("@pages/NotFound"));
+import SignIn from "@pages/SignIn";
+import SignUp from "@pages/SignUp";
+import Content from "@pages/Content";
+import NotFound from "@pages/NotFound";
+
+// const SignIn = React.lazy(() => import("@pages/SignIn")); //로그인
+// const SignUp = React.lazy(() => import("@pages/SignUp")); //회원가입
+// const Content = React.lazy(() => import("@pages/Content")); //Todos가 담긴 페이지
+// const NotFound = React.lazy(() => import("@pages/NotFound"));
 
 const router = createBrowserRouter([
   {
