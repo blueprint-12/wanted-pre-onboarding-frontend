a4d824c753b74b92112ac8d35ba4415cdf5d9b83
blueprint-12 <blueprint12@daum.net>
2023-06-13 02:39:17 +0900

    style: 오류 전으로 복구..

--- src/router.tsx
+++ src/router.tsx
@@ -8,36 +8,33 @@ const SignUp = React.lazy(() => import("@pages/SignUp")); //회원가입
 const Content = React.lazy(() => import("@pages/Content")); //Todos가 담긴 페이지
 const NotFound = React.lazy(() => import("@pages/NotFound"));
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <App />,
-      children: [
-        {
-          index: true,
-          element: <Home />,
-        },
-        {
-          path: "/signin",
-          element: <SignIn />,
-        },
-        {
-          path: "/signup",
-          element: <SignUp />,
-        },
-        {
-          path: "/todo",
-          element: <Content />,
-        },
-        {
-          path: "*",
-          element: <NotFound />,
-        },
-      ],
-    },
-  ],
-  { basename: "process.env.PUBLIC_URL" }
-);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/signin",
+        element: <SignIn />,
+      },
+      {
+        path: "/signup",
+        element: <SignUp />,
+      },
+      {
+        path: "/todo",
+        element: <Content />,
+      },
+      {
+        path: "*",
+        element: <NotFound />,
+      },
+    ],
+  },
+]);
 
 export default router;
