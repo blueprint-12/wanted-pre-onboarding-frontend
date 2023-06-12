feaab848f95cba8edd90ca081670c06b74f2143b
blueprint-12 <blueprint12@daum.net>
2023-06-12 22:47:39 +0900

    design: 로그인 CSS

---
+++ src/pages/SignIn/styles.tsx
@@ -0,0 +1,24 @@
+import styled from "@emotion/styled";
+
+export const FormCont = styled.form`
+  display: flex;
+  flex-direction: column;
+  min-width: 20vw;
+`;
+
+export const Input = styled.input`
+  padding: 5px;
+  margin: 5px 0;
+`;
+
+export const BtnWrapper = styled.button`
+  margin: 10px 0;
+  padding: 5px;
+  font-weight: 600;
+`;
+
+export const Error = styled.div`
+  color: #e01e5a;
+  margin: 8px 0 16px;
+  font-weight: bold;
+`;
