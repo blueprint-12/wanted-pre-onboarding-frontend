ece77beb5f0e4ed47f6f50a4b07f9771016c3712
blueprint-12 <blueprint12@daum.net>
2023-07-06 21:36:54 +0900

    style: FIXME 주석 추가
    - vercel 배포시, 컴포넌트 lazy load 문제는 createBrowserRouter 의 생성시점 문제인 거 같음

--- src/router.tsx
+++ src/router.tsx
@@ -8,6 +8,9 @@ import SignUp from "@pages/SignUp";
 import Content from "@pages/Content";
 import NotFound from "@pages/NotFound";
 
+// FIXME: 컴포넌트 레이지 로드시, 배포 페이지에서 라우팅되지 않는 문제는
+// createBrowserRouter과 관련되어 보임
+
 // const SignIn = React.lazy(() => import("@pages/SignIn")); //로그인
 // const SignUp = React.lazy(() => import("@pages/SignUp")); //회원가입
 // const Content = React.lazy(() => import("@pages/Content")); //Todos가 담긴 페이지
